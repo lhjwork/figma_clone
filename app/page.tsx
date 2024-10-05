3f16ee76256eb26a9bb53b522372553730392bc6
lhjwork <jin.come.up.business@gmail.com>
2024-10-05 21:19:16 +0900

    install liveblocks and fabric

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Room } from "./Room";
 export default function Page() {
   return (
     <Room>
+      <div>15:52 https://www.youtube.com/watch?v=oKIThIihv60</div>
       <CollaborativeApp />
     </Room>
   );
