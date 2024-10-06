57edff05fcd6b8b7a6b27dea6803047d749aadc4
lhjwork <jin.come.up.business@gmail.com>
2024-10-06 23:06:38 +0900

    shadcn and global font applied

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { CollaborativeApp } from "./CollaborativeApp";
-import { Room } from "./Room";
 
 
 export default function Page() {
   return (
-    <Room>
-      <div>15:52 https://www.youtube.com/watch?v=oKIThIihv60</div>
-      <CollaborativeApp />
-    </Room>
+      <h1 className="font-xl text-red-500">Liveblocks Figma Clone</h1>
   );
 }
