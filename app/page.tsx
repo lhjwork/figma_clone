403c70ba09c03c6710b492639230728432fdea4e
lhjwork <jin.come.up.business@gmail.com>
2024-10-06 23:39:41 +0900

    h-[100vh] w-full flex justify-center items-center text-center

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Page() {
   return (
-      <h1 className="font-xl text-red-500">Liveblocks Figma Clone</h1>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">      
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+    </div>
+
   );
 }
