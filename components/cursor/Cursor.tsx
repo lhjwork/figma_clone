79edffcd903c007c9339b4f3a934c88aec06908d
lhjwork <jin.come.up.business@gmail.com>
2024-10-31 08:10:35 +0900

    reaction components makings ( 1:06:14 )

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -17,8 +17,13 @@ const Cursor = ({ color, x, y, message }: Props) => {
 
       {/* MESSAGE */}
       {message && (
-        <div>
-          <p className="text-white">{message}</p>
+        <div
+          className="absoulte left-2 top-5 rounded-3xl px-4 py-2"
+          style={{ backgroundColor: color }}
+        >
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">
+            {message}
+          </p>
         </div>
       )}
     </div>
